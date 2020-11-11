21c11489e07180e52e775c43b4d9881fc13ee6a7
Moiz Khan <mmoizkhan780@gmail.com>
2020-11-11 14:39:53 +0500

    Updates

---
+++ precache-manifest.ae971a7b37fa620cf20b40150358b9cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5439172257b8138e3e79094523ef484",
+    "url": "/Expence-App/index.html"
+  },
+  {
+    "revision": "53922b774f2bb373029e",
+    "url": "/Expence-App/static/css/main.415fd707.chunk.css"
+  },
+  {
+    "revision": "4eb25af760c8d902de6e",
+    "url": "/Expence-App/static/js/2.1bda9a3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expence-App/static/js/2.1bda9a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53922b774f2bb373029e",
+    "url": "/Expence-App/static/js/main.91de9738.chunk.js"
+  },
+  {
+    "revision": "ff7b92901fce86e819d2",
+    "url": "/Expence-App/static/js/runtime-main.e4dec5bb.js"
+  }
+]);
